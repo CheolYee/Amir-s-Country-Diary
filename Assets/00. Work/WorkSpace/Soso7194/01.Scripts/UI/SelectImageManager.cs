5ce51d9ac73db11f2bf1e56f8492c1e4d80e6100
soso7194 <wons5215@naver.com>
2025-12-23 15:26:18 +0900

    feat Tilte

---
+++ Assets/00. Work/WorkSpace/Soso7194/01.Scripts/UI/SelectImageManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace _00._Work.WorkSpace.Soso7194._01.Scripts.UI
+{
+    public class SelectImageManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _image;
+        
+        public void Enter(GameObject owner)
+        {
+            _image.SetActive(true);
+            _image.transform.position = owner.transform.position;
+        }
+        
+        public void Exit() => _image.SetActive(false);
+    }
+}
